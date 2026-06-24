dddd5c0ea6a9cbd0f04952bb20431907122c9959
jamailia <jamailia@pm.me>
2026-06-24 13:58:51 +0200

    adding test for arm architecture

--- tests/integration.rs
+++ tests/integration.rs
@@ -40,8 +40,30 @@ mod tests {
     fn test_aarch64() {
         //simd = true and architecture arch
 
-        //TODO
-        assert!(false);
+        //we chose to have the files in specific folder to ensure reliability
+        let opts = Options {
+            image: "tests/data/input.jpeg".to_string(),
+            output: "tests/data/output_x86.png".to_string(),
+            tiles: "tests/tiles".to_string(),
+            scaling: 1,
+            tile_size: 25,
+            remove_used: false,
+            verbose: false,
+            simd: true, //what we test
+            num_thread: 1,
+        };
+
+        //copying the value because compute_mosaic consumes the structure
+        let output_file = opts.output.clone();
+        compute_mosaic(opts);
+
+        let reference : RgbImage = ImageReader::open("tests/reference/ground-truth-kit.png".to_string()).unwrap().decode().unwrap().into_rgb8();
+        let output : RgbImage = ImageReader::open(&output_file).unwrap().decode().unwrap().into_rgb8();
+
+        //verify step by step, first presence, then format, then content
+        assert!(std::path::Path::new(&output_file).exists());
+        assert_eq!(reference.dimensions(), output.dimensions());
+        assert_eq!(reference, output);
     }
 
     #[test]
